f87577b0f4d60db43e977242483fbc6fcf8c39b3
Jérémy BOLZONELLA <jeremy.bolzonella@cfast.fr>
2018-04-04 08:45:43 +0200

    Change default parameter

--- libs/src/modal/modal.service.ts
+++ libs/src/modal/modal.service.ts
@@ -10,7 +10,7 @@ export class GSTModal {
         let def = {
             backdrop: false,
             container: '.modal-outer-template-div',
-            keyboard: true,
+            keyboard: false,
             size: '',
             windowClass: ''
         };
